b6faa8a11d6938a101ad133e76833291d4841ced
Ivan <iburimskiy@gmail.com>
2019-11-15 23:30:58 +0300

    Initial Commit

---
+++ sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sort
+//
+//  Created by Ivan Burimskiy on 15/11/2019.
+//  Copyright © 2019 Иван Буримский. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

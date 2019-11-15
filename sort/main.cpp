32ad0c82487e7ae577176f78eb71b0ff6aac0614
Ivan <iburimskiy@gmail.com>
2019-11-16 00:00:12 +0300

    function added

--- sort/main.cpp
+++ sort/main.cpp
@@ -7,11 +7,26 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <iterator>
 
 using namespace std;
 
 template <typename T>
 auto my_sort(T arr) {
+    unsigned int length = arr.size;
+    typename T::iterator arr_begin_iter = arr.begin();
+    T left_arr, right_arr, sorted_left_arr, sorted_right_arr;
+    auto middle_elem_value = *(arr_begin_iter
+                               + (length / 2));
+    for (auto it = arr_begin_iter; it != arr.end(); ++it) {
+        if (*it < middle_elem_value) {
+            left_arr.push_back(*it);
+        } else {
+            right_arr.push_back(*it);
+        }
+    }
+    
     return 0;
 }
 

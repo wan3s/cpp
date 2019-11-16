e1fce3a73eb0eee445f5166a52df19f8b9f916b9
Ivan <iburimskiy@gmail.com>
2019-11-16 13:03:20 +0300

    small refactoring

--- sort/main.cpp
+++ sort/main.cpp
@@ -9,8 +9,8 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
-#include <algorithm>
 #include <string>
+#include <array>
 
 #define N 10
 #define MIN_ELEM_VALUE -5
@@ -19,33 +19,42 @@
 using namespace std;
 
 template<typename T>
-T get_rand_num (T min, T max) {
+T
+get_rand_num (T min, T max) {
     return rand() % (max - min) + min;
 }
 
 template<typename T>
-auto init_arr(T &arr, int elems_num) {
+void
+init_vec(T &vec, int elems_num) {
     for (int i = 0; i < elems_num; ++i){
-        arr.push_back(get_rand_num(MIN_ELEM_VALUE, MAX_ELEM_VALUE));
+        vec.push_back(get_rand_num(MIN_ELEM_VALUE, MAX_ELEM_VALUE));
     }
-    return arr;
 }
 
 template<typename T>
 void
-print_arr(T *arr, string label = "") {
+init_arr(T &arr) {
+    for (auto iter = arr.begin(); iter != arr.end(); ++iter) {
+        *iter = get_rand_num(MIN_ELEM_VALUE, MAX_ELEM_VALUE);
+    }
+}
+
+template<typename T>
+void
+print_template(T *arr, string label = "") {
     if (label.length() > 0) {
         cout << label << ": ";
     }
-    for (auto &i : *arr) {
-        cout << i << " ";
+    for (auto iter = (*arr).begin(); iter != (*arr).end(); ++iter) {
+        cout << *iter << " ";
     }
     cout << endl;
 }
 
 template <typename T>
 void
-my_sort (T left, T right) {
+my_quick_sort (T left, T right) {
     auto i = left, j = right;
     auto cmp = *i;
     do {
@@ -61,21 +70,27 @@ my_sort (T left, T right) {
         }
     } while (i <= j);
     if (left < j) {
-        my_sort(left, j);
+        my_quick_sort(left, j);
     }
     if (i < right) {
-        my_sort(i, right);
+        my_quick_sort(i, right);
     }
 }
 
-int main(void) {
+int
+main(void) {
     vector<int> my_vec;
-    init_arr(my_vec, N);
-    vector<int>::iterator begin = my_vec.begin(), end = my_vec.end();
+    array<float, N> my_arr;
     
-    print_arr(&my_vec, "VEC");
-    my_sort(begin, end);
-    print_arr(&my_vec, "RES");
+    init_vec(my_vec, N);
+    print_template(&my_vec, "VEC");
+    my_quick_sort(my_vec.begin(), my_vec.end());
+    print_template(&my_vec, "RES");
+    
+    init_arr(my_arr);
+    print_template(&my_arr, "ARR");
+    my_quick_sort(my_arr.begin(), my_arr.end());
+    print_template(&my_arr, "RES");
     
     return 0;
 }

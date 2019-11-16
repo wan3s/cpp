4de5ea45ae53ab6199d5383dbb23ee2093fce9c0
Ivan <iburimskiy@gmail.com>
2019-11-16 12:39:05 +0300

    works for different templates

--- sort/main.cpp
+++ sort/main.cpp
@@ -9,12 +9,44 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <algorithm>
+#include <string>
+
+#define N 10
+#define MIN_ELEM_VALUE -5
+#define MAX_ELEM_VALUE 5
 
 using namespace std;
 
-//template <typename T>
-void my_sort (vector<int>::iterator &left, vector<int>::iterator &right) {
-    vector<int>::iterator i = left, j = right;
+template<typename T>
+T get_rand_num (T min, T max) {
+    return rand() % (max - min) + min;
+}
+
+template<typename T>
+auto init_arr(T &arr, int elems_num) {
+    for (int i = 0; i < elems_num; ++i){
+        arr.push_back(get_rand_num(MIN_ELEM_VALUE, MAX_ELEM_VALUE));
+    }
+    return arr;
+}
+
+template<typename T>
+void
+print_arr(T *arr, string label = "") {
+    if (label.length() > 0) {
+        cout << label << ": ";
+    }
+    for (auto &i : *arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void
+my_sort (T left, T right) {
+    auto i = left, j = right;
     auto cmp = *i;
     do {
         while (*i < cmp && i < right) {
@@ -24,11 +56,8 @@ void my_sort (vector<int>::iterator &left, vector<int>::iterator &right) {
             --j;
         }
         if (i <= j) {
-            auto tmp = *i;
-            *i = *j;
-            *j = tmp;
-            ++i;
-            --j;
+            iter_swap(i, j);
+            static_cast<void>(++i), --j;
         }
     } while (i <= j);
     if (left < j) {
@@ -40,12 +69,13 @@ void my_sort (vector<int>::iterator &left, vector<int>::iterator &right) {
 }
 
 int main(void) {
-    vector<int> my_vec = {5, 4, 3, 2, 1};
+    vector<int> my_vec;
+    init_arr(my_vec, N);
     vector<int>::iterator begin = my_vec.begin(), end = my_vec.end();
+    
+    print_arr(&my_vec, "VEC");
     my_sort(begin, end);
-    for (auto &i : my_vec) {
-        cout << i << " ";
-    }
-    cout << endl;
+    print_arr(&my_vec, "RES");
+    
     return 0;
 }
